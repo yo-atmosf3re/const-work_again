5c749394a0d7664f6fb7dbecc424fdf592e5717a
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-11-16 13:18:11 +0300

    feat: протестировано API с бэка, подготовка к внедрению axios'a в проект;

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -28,10 +28,107 @@ const DataRow = [
 ]
 
 const Main = () => {
+   // useEffect(() => {
+   //    const eID = axios.post(`http://185.244.172.108:8081/v1/outlay-rows/entity/create`)
+   //    axios.get(`http://185.244.172.108:8081/v1/outlay-rows/entity/${eID}/row/list`)
+   // }, [])
+   const url = `http://185.244.172.108:8081/v1/outlay-rows/entity/create`
+
    useEffect(() => {
-      const eID = axios.post(`http://185.244.172.108:8081/v1/outlay-rows/entity/create`)
-      axios.get(`http://185.244.172.108:8081/v1/outlay-rows/entity/${eID}/row/list`)
+      const fetchData = async () => {
+         try {
+            // ** Create entity - completed
+            const entityCreate = await fetch(url, {
+               method: "POST",
+               mode: "cors",
+               headers: {
+                  "Content-Type": "application/json",
+                  "Accept": "application/json",
+               },
+            })
+            const entity = await entityCreate.json()
+            const eID = entity.id
+            console.log(entity, 'Creating entity')
+            // ** Create row inside the entity - completed
+            const createRowInEntity = await fetch(`http://185.244.172.108:8081/v1/outlay-rows/entity/${eID}/row/create`, {
+               method: 'POST',
+               mode: "cors",
+               headers: {
+                  "Content-Type": "application/json",
+                  "Accept": "application/json",
+               },
+               body: JSON.stringify({
+                  equipmentCosts: 0,
+                  estimatedProfit: 0,
+                  machineOperatorSalary: 0,
+                  mainCosts: 0,
+                  materials: 0,
+                  mimExploitation: 0,
+                  overheads: 0,
+                  parentId: null,
+                  rowName: 'Test',
+                  salary: 0,
+                  supportCosts: 0
+               })
+            })
+            const newRow = await createRowInEntity.json()
+            console.log(newRow, 'New row')
+            // ** Get data about entity - completed
+            const getData = await fetch(`http://185.244.172.108:8081/v1/outlay-rows/entity/${eID}/row/list`, {
+               method: 'GET',
+               mode: "cors",
+               headers: {
+                  "Content-Type": "application/json",
+                  "Accept": "application/json",
+               },
+            })
+            const getDataResult = await getData.json()
+            console.log(getDataResult, 'Data row')
+            const rID = getDataResult.map((m: any) => m.id)
+            console.log(rID[0], 'Row Id')
+            // ** Update row - completed
+            const updateRow = await fetch(`http://185.244.172.108:8081/v1/outlay-rows/entity/${eID}/row/${rID[0]}/update`, {
+               method: 'POST',
+               mode: "cors",
+               headers: {
+                  "Content-Type": "application/json",
+                  "Accept": "application/json",
+               },
+               body: JSON.stringify({
+                  equipmentCosts: 1,
+                  estimatedProfit: 1,
+                  machineOperatorSalary: 1,
+                  mainCosts: 1,
+                  materials: 1,
+                  mimExploitation: 0,
+                  overheads: 0,
+                  parentId: null,
+                  rowName: 'Test 1',
+                  salary: 0,
+                  supportCosts: 0
+               })
+            })
+            const updateResult = await updateRow.json()
+            console.log(updateResult, 'Updating row')
+            // ** Delete row - completed
+            const deleteRow = await fetch(`http://185.244.172.108:8081/v1/outlay-rows/entity/${eID}/row/${rID}/delete`, {
+               method: 'DELETE',
+               mode: "cors",
+               headers: {
+                  "Content-Type": "application/json",
+                  "Accept": "application/json",
+               },
+            })
+            const deleteRowResponse = await deleteRow.json()
+            console.log(deleteRowResponse, 'Delete row')
+            console.log(getDataResult, 'Data row again')
+         } catch (error) {
+            console.log(error)
+         }
+      }
+      fetchData()
    }, [])
+
 
    return (
       <div className='main'>
@@ -44,7 +141,7 @@ const Main = () => {
             <div className='container__items'>
                {DataRow.map((t, i) => {
                   return <div key={i} className="items__row">
-                     {IconList.map(i => t.id === i.id && i.icon)}
+                     <>{IconList.map(i => t.id === i.id && i.icon)}</>
                      <div className="row__title-work" > {<EditableSpan value={t.rowName} />}</div >
                      <div className="row__salary">{<EditableSpan value={t.salary} />}</div>
                      <div className="row__equipment-costs">{<EditableSpan value={t.equipment} />}</div>
